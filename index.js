5a9437a5f9f3313849169059fd5666f1e0d67d84
dzhangarnnn <dzhangarnnn@gmail.com>
2022-10-26 21:14:04 +0700

    made changes in feature/user-acces

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log('Hello world');
 console.log('Hello world1');
+
+const age = 28;
+if (age >= 18) {
+    console.log('Вы совершеннолетний');
+} else {
+    console.log('Вы Не совершеннолетний');
+}
